d6c0e73669b5431073461c7364493efbfb84d435
T3nG <dengfixanros@gmail.com>
2023-01-04 21:46:02 +0800

    bug fix

--- 20221202_machine_learning/15_GD_learning_rate_2/RMSP.py
+++ 20221202_machine_learning/15_GD_learning_rate_2/RMSP.py
@@ -12,8 +12,9 @@ class RMSP(AdaGrad):
         self.a_old = self.a
         self.b_old = self.b
         grad_a, grad_b = self.gradient()
-        self.sum_grad_a += self.rho * self.sum_grad_a + (1 - self.rho) * grad_a ** 2
-        self.sum_grad_b += self.rho * self.sum_grad_b + (1 - self.rho) * grad_b ** 2
+        self.sum_grad_a = self.rho * self.sum_grad_a + (1 - self.rho) * grad_a ** 2
+        self.sum_grad_b = self.rho * self.sum_grad_b + (1 - self.rho) * grad_b ** 2
+        # 梯度更新
         self.a = self.a_old - (self.lr / (np.sqrt(self.sum_grad_a) + self.e)) * grad_a
         self.b = self.b_old - (self.lr / (np.sqrt(self.sum_grad_b) + self.e)) * grad_b
         self.loss = self.mse()
